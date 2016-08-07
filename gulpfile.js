94291e172e24bb5196e591eca1405c0e416d8110
Dmitrydizumskiy@eisgroup.com <idimaster@gmail.com>
2016-08-06 17:37:01 -0700

    Added declarations and source maps

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,20 @@
 const gulp = require('gulp');
 const ts = require('gulp-typescript');
 const babel = require('gulp-babel');
+const merge = require('merge2')
+const sourcemaps = require('gulp-sourcemaps');
+
 const tsProject = ts.createProject('tsconfig.json');
 
 gulp.task('default', function() {
-    return tsProject.src()
-        .pipe(ts(tsProject))
-        .js.pipe(babel())
-        .pipe(gulp.dest('lib'));
+    const result = tsProject.src()
+        .pipe(sourcemaps.init())
+        .pipe(ts(tsProject));
+
+    return merge([
+        result.dts.pipe(gulp.dest('dist/definitions')),
+        result.js.pipe(babel())
+            .pipe(sourcemaps.write())
+            .pipe(gulp.dest('dist/js'))
+    ]);
 });
