015335a4db1a98beadf169b9fe06c2243ee57841
Dmitrydizumskiy@eisgroup.com <idimaster@gmail.com>
2016-08-19 19:05:12 -0700

    Brand initial implementation

--- src/navigation/Brand.tsx
+++ src/navigation/Brand.tsx
@@ -5,7 +5,7 @@ export class Brand extends React.Component<any, any> {
     render(): React.ReactElement<any> {
         return (
             <a href="/" className="navbar-brand">
-                <img className="navbar-brand-icon" src="/assets/img/logo-alt.svg" alt=""/><img className="navbar-brand-name" src="/assets/img/brand-alt.svg" alt="PatternFly Enterprise Application" />
+                {this.props.children}
             </a>)
     }
 }
