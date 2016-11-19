f598e66bdd653f3684b37944023885618b6f42c9
Dmitry Izumskiy <idimaster@gmail.com>
2016-11-19 00:34:39 -0700

    Implemented Wizard components

---
+++ src/wizard/StepTitle.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react'
+
+import {CommandPayload, OnSelectCallback} from '../events';
+
+export class StepTitle extends React.Component<{active?: boolean, number?: string, title:string, onSelect?: OnSelectCallback, payload?: CommandPayload}, any> {
+    static propTypes() {
+        return {
+            active: React.PropTypes.bool,
+            number: React.PropTypes.string,
+            title: React.PropTypes.string,
+            onSelect: React.PropTypes.func,
+            payload: React.PropTypes.object
+        }
+    }
+    
+    private handleSelect = () => {
+        if (this.props.onSelect) {
+            this.props.onSelect(this.props.payload);
+        }
+    }
+    
+    render(): React.ReactElement<any> {
+        var style = "list-group-item";
+        if (this.props.active) style += ' active';
+        return <li className={style}>
+            <a onClick={this.handleSelect}>
+                <span className="wizard-pf-substep-number">{this.props.number}</span>
+                <span className="wizard-pf-substep-title">{this.props.title}</span>
+            </a>
+        </li>
+    }
+}
