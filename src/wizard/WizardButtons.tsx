bb22e44822c5786d8712c7d440278bc23d316f90
idimaster <idimaster@gmail.com>
2016-11-28 10:48:22 -0700

    Fixed Wizard Buttons - event handler for cancel and prevent onClick on disabled buttons

--- src/wizard/WizardButtons.tsx
+++ src/wizard/WizardButtons.tsx
@@ -2,13 +2,14 @@ import * as React from 'react'
 
 import {CommandPayload, OnSelectCallback} from '../events';
 
-export class WizardButtons extends React.Component<{first?: boolean, last?: boolean, onNext?: OnSelectCallback, onBack?: OnSelectCallback, payload?: CommandPayload}, any> {
+export class WizardButtons extends React.Component<{first?: boolean, last?: boolean, onNext?: OnSelectCallback, onBack?: OnSelectCallback, onCancel?: OnSelectCallback, payload?: CommandPayload}, any> {
     static propTypes() {
         return {
             first: React.PropTypes.bool,
             last: React.PropTypes.bool,
             onNext: React.PropTypes.func,
             onBack: React.PropTypes.func,
+            onCancel: React.PropTypes.func,
             payload: React.PropTypes.object
         }
     }
@@ -24,16 +25,24 @@ export class WizardButtons extends React.Component<{first?: boolean, last?: bool
             this.props.onBack(this.props.payload);
         }
     };
+
+    private handleCancel = () => {
+        if (this.props.onCancel) {
+            this.props.onCancel(this.props.payload);
+        }
+    };
     
     render(): React.ReactElement<any> {
         let styleBack = this.props.first ? "btn btn-default disabled" : "btn btn-default";
+        let optBack = this.props.first ? {disabled: 'disabled'} : {};
         let styleNext = this.props.last ? "btn btn-primary disabled" : "btn btn-primary";
+        let optNext = this.props.last ? {disabled: 'disabled'} : {};
         return  <div className="modal-footer wizard-pf-footer">
-            <button type="button" className="btn btn-default btn-cancel">Cancel</button>
+            <button type="button" onClick={this.handleCancel} className="btn btn-default btn-cancel">Cancel</button>
              {this.props.children}
-            <button type="button" onClick={this.handleBack} className={styleBack}>
+            <button type="button" onClick={this.handleBack} {...optBack} className={styleBack}>
                  <span className="i fa fa-angle-left"></span>Back</button>
-            <button type="button" onClick={this.handleNext} className={styleNext}>Next<span className="i fa fa-angle-right"></span></button>
+            <button type="button" onClick={this.handleNext} {...optNext} className={styleNext}>Next<span className="i fa fa-angle-right"></span></button>
         </div>
     }
 }
